3bca6ec7824ad3307f492cab4101a62bde754443
quentindeschamps <quentin.deschamps@externe.maif.fr>
2021-06-14 11:18:24 +0200

    Initial Commit

---
+++ VisionWWDC2021/VisionWWDC2021App.swift
@@ -0,0 +1,17 @@
+//
+//  VisionWWDC2021App.swift
+//  VisionWWDC2021
+//
+//  Created by Quentin Deschamps on 14/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct VisionWWDC2021App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VisionWWDC2021Tests/VisionWWDC2021Tests.swift
@@ -0,0 +1,33 @@
+//
+//  VisionWWDC2021Tests.swift
+//  VisionWWDC2021Tests
+//
+//  Created by Quentin Deschamps on 14/06/2021.
+//
+
+import XCTest
+@testable import VisionWWDC2021
+
+class VisionWWDC2021Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
